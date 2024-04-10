cfc9b44025d6bdd97143293311a79690a427486b
kawtar <kawtarennabirha@gmail.com>
2024-04-10 16:35:04 +0000

    API_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" requests number of subscribers from reddit api """
+import re
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers for a given subreddit"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/about/"
+    res = requests.get(url)
+    html = res.text
+    pattern = r'subscribers="([^"]*)"'
+    subscribers = re.search(pattern, html)
+
+    if subscribers:
+        return int(subscribers[0][13:-1])
+    else:
+        return 0
